21f9f337901c983a10f0f043f6eede3ae0bdad95
xiangxingxing <buaaxxx1995@gmail.com>
2020-08-07 14:27:11 +0800

    add accessing-data-mysql

---
+++ SpringBoot-MySQL/src/main/java/me/levi/repository/UserRepository.java
@@ -0,0 +1,8 @@
+package me.levi.repository;
+
+import me.levi.pojo.User;
+import org.springframework.data.repository.CrudRepository;
+
+public interface UserRepository extends CrudRepository<User, Integer> {
+
+}
